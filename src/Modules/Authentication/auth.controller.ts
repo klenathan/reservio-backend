23d6f0ebb5ea9a33ca60fbb079c592488ed900fc
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2023-03-30 20:46:59 +0700

    [FEAT] Image Handling

--- src/Modules/Authentication/auth.controller.ts
+++ src/Modules/Authentication/auth.controller.ts
@@ -1,3 +1,4 @@
+import CustomError from "@/Errors/CustomError";
 import { PrismaClient } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import BaseController from "../Base/BaseController";
@@ -17,6 +18,22 @@ export default class AuthController extends BaseController {
         req.body.username,
         req.body.password
       );
+      return res.json(result);
+    } catch (e) {
+      next(e);
+    }
+  };
+  signup = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if (req.files?.length == 0) {
+        throw new CustomError(
+          "INVALID_AVATAR",
+          "Please include an avatar",
+          400
+        );
+      }
+      const avatar = req.files as unknown as Express.Multer.File[];
+      let result = await this.service.handleSignUp(avatar[0]);
       return res.json(result);
     } catch (e) {
       next(e);
--- src/Modules/Authentication/auth.service.ts
+++ src/Modules/Authentication/auth.service.ts
@@ -4,6 +4,10 @@ import { PrismaClient } from "@prisma/client";
 import BaseService from "../Base/BaseService";
 import { comparePassword } from "./Utils/passwordUtils";
 
+
+import { PutObjectCommand, S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
+import CustomError from "@/Errors/CustomError";
+
 export default class AuthService extends BaseService {
   public constructor(db: PrismaClient) {
     super(db);
@@ -31,5 +35,35 @@ export default class AuthService extends BaseService {
       accessToken: accessToken,
       refreshToken: refreshToken,
     };
+  };
+
+  handleSignUp = async (avatar: Express.Multer.File) => {
+    const config = {
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRETE_ACCESS_KEY,
+      region: "ap-southeast-1",
+    } as S3ClientConfig;
+
+    const client = new S3Client(config);
+
+    // const imgBlob = fs.readFileSync(imagePath)
+    console.log(avatar);
+    
+
+    const command = new PutObjectCommand({
+      Bucket: "reservio",
+      Key: avatar.originalname,
+      Body: avatar.buffer,
+    });
+
+    try {
+      const response = await client.send(command);
+      console.log(response);
+    } catch (err: any) {
+      console.log(err);
+      throw new CustomError(err.name, err.message, 400);
+    }
+
+    return { message: "done" };
   };
 }
